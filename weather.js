15cd84a63dd3dce40215da7470eed5df4039d193
Alexander Weber <Alexander.Weber@liebherr.com>
2020-05-29 15:32:26 +0200

    xxxx

--- weather.js
+++ weather.js
@@ -25,6 +25,26 @@ function UpdateWeatherData(lat, lng){
         ///
     });
 
+
+    $.getJSON(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=hourly,minutely&appid=a0e01a58965bfa8692262f34f8375f72&units=metric`,function(json){
+        
+        var labels = [];
+        var dataMin = [];
+        var dataMax = [];
+
+
+        for(i = 0; i < json.daily.length; i++){
+            dataMin.push(json.daily[i].temp.min);
+            dataMax.push(json.daily[i].temp.max);
+            labels.push(i);
+        }
+    
+        UpdateChart("myChart", labels, dataMin, dataMax)
+
+    });
+
+
+
 }
 
 
